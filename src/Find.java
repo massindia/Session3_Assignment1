2695a28202ec1b773dd8fa9a2e8181446b2ff4ab
massindia <mr.perfect@live.co.uk>
2016-12-06 20:30:22 +0530

    Session3_Assignemnt1
    Find Error

---
+++ src/Find.java
@@ -0,0 +1,16 @@
+public class Test{ 
+public static void main(String[] args) {
+	
+print(10); 
+} 
+static void print (int x) { 
+System.out.println (x);
+System.exit(0); 
+}
+}
+
+/*So, The correct answer for this problem is 
+    a) Compiler Error.
+ 
+Since this problem doen't have a main method , it will give a compiler 
+error**/
